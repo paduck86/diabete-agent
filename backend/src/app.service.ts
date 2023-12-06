edf586fe89bb9da2ec16b7b34d56314642085326
정진석 <jeongjinseog@Jake.local>
2023-12-06 13:43:04 -0800

    change directory structures

---
+++ backend/src/app.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+
+//@Injectable()
+export class AppService {
+    // Method to return example data
+    getExampleData(name: string): string {
+        return `Hello$(name ? ' ' + name : ''), this is your example data!`;
+    }
+}
+
+
+
